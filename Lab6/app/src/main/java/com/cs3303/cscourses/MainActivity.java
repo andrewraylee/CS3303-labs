9b66ae23856975d43fd510f138bf87eabe5a68ed
Andrew Lee <arlee1@buffs.wtamu.edu>
2020-03-24 15:23:10 -0500

    Lab 6 work completed (comment added)

--- Lab6/app/src/main/java/com/cs3303/cscourses/MainActivity.java
+++ Lab6/app/src/main/java/com/cs3303/cscourses/MainActivity.java
@@ -3,7 +3,12 @@ package com.cs3303.cscourses;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/*
+    Author: Andrew Lee
+    Assignment: Lab 6
+    Name: CS Courses
+    Date: 3/24/19
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
